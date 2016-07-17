19dd84aab3d2e79055914325468c1f73458b9018
Martin Rodin <rodin.martin@gmail.com>
2016-07-17 17:52:31 +0200

    Add project files.

---
+++ DotNetGram/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetGram.Startup))]
+namespace DotNetGram
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
